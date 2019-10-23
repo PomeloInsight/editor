6bc8c029cc6ea25d4de850b3573a56ae1bf7dc4b
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2019-10-23 23:58:48 +0800

    add logo

--- pages/editor/statusbar/logo.tsx
+++ pages/editor/statusbar/logo.tsx
@@ -3,9 +3,7 @@ import * as React from "react";
 import * as css from "./logo.scss";
 
 const Logo = (
-    <div className={ css.logo }>
-        PomeloInsight
-    </div>
+    <div className={ css.logo }/>
 );
 
 export default Logo;
